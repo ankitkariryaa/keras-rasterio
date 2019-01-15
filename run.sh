ffce64260d40a932a77ecf6c1ed83e0caa36cb28
boba-fett <hci.uni.bremen@gmail.com>
2019-01-15 18:38:50 +0100

    Fix the image name

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run --runtime=nvidia -p 8888:8888 -v $PWD:/notebooks -d ankitkariryaa:keras
+docker run --runtime=nvidia -p 8888:8888 -v $PWD:/notebooks -d ankitkariryaa/keras-rasterio:latest-gpu-py3
+docker logs container-name-here
